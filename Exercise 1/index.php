8d9e0914b85761698e2c6a73283015667b8aad97
trieu.long <long.trieu.hr@gmail.com>
2022-01-25 16:56:19 +0700

    update

--- Exercise 1/index.php
+++ Exercise 1/index.php
@@ -1,9 +1,34 @@
 <?php
-function createArray($number){
+function createArray($number)
+{
     $array = [];
-    for ($i = 0 ; $i<$number;$i++){
-        $array[] = rand(1,100);
+    for ($i = 0; $i < $number; $i++) {
+        $array[] = rand(1, 100);
     }
     return $array;
 }
-print_r(createArray(5));
+
+$array = createArray(5);
+print_r($array);
+echo "<br>";
+
+
+function sortArray($array)
+{
+    for ($i = 0; $i< count($array) ; $i++){
+        $min = $array[$i];
+        $index = $i;
+        for ($j = $i +1 ; $j < count($array); $j++){
+            if ($min > $array[$j]){
+                $min = $array[$j];
+                $index =$j;
+            }
+        }
+        $temp = $array[$i];
+        $array[$i] = $min;
+        $array[$index] = $temp;
+    }
+    return $array;
+}
+
+print_r(sortArray($array));
