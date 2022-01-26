2860fd2e52bb8611f57f898f27bb5fd072c1735d
trieu.long <long.trieu.hr@gmail.com>
2022-01-26 09:44:09 +0700

    update

--- Exercise 2/index.php
+++ Exercise 2/index.php
@@ -33,7 +33,7 @@ function findMax($array): string
     $max = $array[0];
     for ($i = 0; $i < count($array); $i++) {
         if ($array[$i] > $max) {
-           $max = $array[$i];
+            $max = $array[$i];
         }
     }
     return " So lon nhat la : " . $max . "<br>";
@@ -41,15 +41,108 @@ function findMax($array): string
 
 print_r(findMax($array)) . "<br>";
 
-function findMin($array){
+function findMin($array)
+{
     $min = $array[0];
-    for ( $i = 0 ; $i < count($array); $i++ ){
-        if ($array[$i] < $min){
-            $min =  $array[$i];
+    for ($i = 0; $i < count($array); $i++) {
+        if ($array[$i] < $min) {
+            $min = $array[$i];
         }
     }
     return " So nho nhat trong mang la : " . $min;
 }
 
 print_r(findMin($array));
+echo "<br>";
+
+function evenMax($array)
+{
+    $max = $array[0];
+    for ($i = 0; $i < count($array); $i++) {
+        if ($array[$i] % 2 == 0) {
+            if ($array[$i] > $max) {
+                $max = $array[$i];
+            }
+        }
+    }
+    return " So nguyen duong lon nhat trong mang la : " . $max;
+}
+
+print_r(evenMax($array));
+echo "<br>";
+
+function evenMin($array)
+{
+    $min = $array[0];
+    for ($i = 0; $i < count($array); $i++) {
+        if ($array[$i] % 2 == 0) {
+            if ($array[$i] < $min) {
+                $min = $array[$i];
+            }
+        }
+    }
+    return " So nguyen nho nhat trong mang la : " . $min;
+}
+
+print_r(evenMin($array));
+echo "<br>";
+
+
+function sumArray($array)
+{
+    $sum = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        $sum += $array[$i];
+    }
+    return " Tong cua mang la : " . $sum;
+}
+
+print_r(sumArray($array));
+echo "<br>";
+
+
+function avgSum($array)
+{
+    $sum = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        $sum += $array[$i];
+    }
+    return " Trung binh tong cua mang la : " . floor($sum / count($array));
+}
+
+print_r(avgSum($array));
+echo "<br>";
+
+function maxThanAvgSum($array)
+{
+    $arr = [];
+    $sum = 0;
+    for ($i = 0; $i < count($array); $i++) {
+        $sum += $array[$i];
+    }
+    $avg = $sum / count($array);
+    for ($i = 0 ; $i <count($array) ; $i++){
+        if ($array[$i] > $avg){
+            $arr[] = $array[$i];
+        }
+    }
+    return $arr;
+
+}
+
+
+echo " Cac so lon hon trung binh cong la :" . "<br>";
+print_r(maxThanAvgSum($array)) ;
+echo "<br>";
+
+
+function sortIn($array){
+
+    sort($array);
+    return $array;
+
+}
+echo " Mang sap xep theo chieu tang dan la : " . "<br>";
+print_r( sortIn($array));
+echo "<br>";
 
